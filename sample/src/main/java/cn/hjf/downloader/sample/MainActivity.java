7d4045260750cdbfa73d3e8251393cc0b749127b
xyhuangjinfu <630569027@qq.com>
2017-08-03 11:06:48 +0800

    Fix sample.

--- sample/src/main/java/cn/hjf/downloader/sample/MainActivity.java
+++ sample/src/main/java/cn/hjf/downloader/sample/MainActivity.java
@@ -2,23 +2,30 @@ package cn.hjf.downloader.sample;
 
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ProgressBar;
 
 import java.io.File;
 
+import cn.hjf.downloader.ErrorListener;
 import cn.hjf.downloader.Listener;
 import cn.hjf.downloader.http.HttpDownloader;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar pb;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        HttpDownloader httpDownloader = new HttpDownloader();
+        pb = (ProgressBar) findViewById(R.id.pb);
 
+        HttpDownloader httpDownloader = new HttpDownloader();
 
         String urlStr = "http://imgsrc.baidu.com/imgad/pic/item/267f9e2f07082838b5168c32b299a9014c08f1f9.jpg";
         String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()
@@ -37,8 +44,16 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 @Override
-                public void onProgress(double progress) {
-                    Log.e("O_O", "onProgress : " + progress);
+                public void onProgress(final long total, final long progress) {
+//                    Log.e("O_O", "onProgress, total : " + total + "  -  progress : " + progress);
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            pb.setMax(100);
+                            pb.setProgress((int) (progress * 100 / total));
+                        }
+                    });
                 }
 
                 @Override
@@ -46,13 +61,19 @@ public class MainActivity extends AppCompatActivity {
                     Log.e("O_O", "onFinish");
                 }
 
+            }, new ErrorListener() {
                 @Override
-                public void onError(String error) {
-                    Log.e("O_O", "onError : " + error);
+                public void onInvalidUrl(String urlStr) {
+                    Log.e("O_O", "onInvalidUrl : " + urlStr);
+                }
+
+                @Override
+                public void onError(Exception error) {
+                    Log.e("O_O", "onError : " + error.getMessage());
                 }
             });
         } catch (Exception e) {
-             e.printStackTrace();
+            e.printStackTrace();
         }
 
     }
