017f76433e62487dc31b85ce769532c5b6cba507
xyhuangjinfu <630569027@qq.com>
2017-08-10 15:29:38 +0800

    Add main thread notifier.

---
+++ library/src/main/java/cn/hjf/downloader/MainThreadEventNotifier.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2017 huangjinfu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.hjf.downloader;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by huangjinfu on 2017/8/10.
+ */
+
+final class MainThreadEventNotifier {
+
+    private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+    public void notifyWait(@NonNull final Task task) {
+        mainThreadHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                task.getListener().onWait(task);
+            }
+        });
+    }
+
+    public void notifyStart(@NonNull final Task task) {
+        mainThreadHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                task.getListener().onStart(task);
+            }
+        });
+    }
+
+    public void notifyProgress(@NonNull final Task task, @NonNull final Progress progress) {
+        mainThreadHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                task.getListener().onProgress(task, progress);
+            }
+        });
+    }
+
+    public void notifyStop(@NonNull final Task task) {
+        mainThreadHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                task.getListener().onStop(task);
+            }
+        });
+    }
+
+    public void notifyFinish(@NonNull final Task task) {
+        mainThreadHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                task.getListener().onFinish(task);
+            }
+        });
+    }
+
+    public void notifyDelete(@NonNull final Task task) {
+        mainThreadHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                task.getListener().onDelete(task);
+            }
+        });
+    }
+
+    public void notifyError(@NonNull final Task task, @NonNull final Exception error) {
+        mainThreadHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                task.getErrorListener().onError(task, error);
+            }
+        });
+    }
+}
